954c98bb7ab960d64ac4ebc13ef75392911801ce
milalu <milalu@DESKTOP-3FN8DBJ>
2017-12-15 12:37:36 +0100

    maquette de l'entete

--- vues/v_entete.php
+++ vues/v_entete.php
@@ -1,8 +1,26 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+          <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet">
+    </head>
+    <body>
+        <div class = "container-fluid">
+            <div class="row">
+                <div class="col-md-4 col-sm-4 col-xs-4">
+                    <img src="images/images.jpg" class="image-rounded">
+                </div>
+            </div>
+            <nav class="navbar navbar-inverse">
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="#">Accueil </a></li>
+                    <li><a href="#">Voir le catalogue des vols</a></li>
+                    <li><a href="#">voir les reservations</a></li>
+                </ul>
+            </nav>
+        </div>
+        <script src ="http://code.jquery.com/jquery.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+    </body>
+</html>
